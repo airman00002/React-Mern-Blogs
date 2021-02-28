73e04663ef8832de86d21d6688d86073f6030a1e
Airman00002 <Natthaphon0003@gmail.com>
2021-02-28 14:55:43 +0700

    something mongodb

--- back-end/database/database.js
+++ back-end/database/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-     db:'mongodb+srv://admin:123456abcd@react-mern-node.z6bkl.mongodb.net/reactBlogs?retryWrites=true&w=majority'
+     db:'mongodb+srv://admin:123456abcd@react-mern-node.z6bkl.mongodb.net/ReactBlogs?retryWrites=true&w=majority'
 }
